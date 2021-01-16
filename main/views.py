c98bfcd6d54cb8b0b88bc9732c9768898696fd58
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-01-15 20:05:05 -0500

    Initial Commit

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# CREATE VIEWS HERE
+
+
