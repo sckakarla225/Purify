068dd0ba8a2cc5ff8d5076541b306766c96ebe24
Samhith Kakarla <samhith.kakarla@gmail.com>
2021-01-30 18:03:43 -0500

    Created Home Page and Menu Navigation

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -3,24 +3,34 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../App.css';
 
-export const Menu = () => {
+// UI
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+
+export const Menu = ({ closeMenu }) => {
     return (
         <div>
-            <img src={logo} className="menu-logo" />
-            <h1 className="menu-header">PURIFY</h1>
-            <div className="menu-buttons-container">
-                <Link 
-                    to="/"
-                    className="menu-buttons"
-                >
-                    HOME
-                </Link>
-                <Link 
-                    to="/map"
-                    className="menu-buttons"
-                >
-                    WATER MAP
-                </Link>
+            <div className="row">
+                <MenuRoundedIcon
+                    style={{ fontSize: 50 }}
+                ></MenuRoundedIcon>
+            </div>
+            <div className="row">
+                <img src={logo} className="menu-logo" />
+                <h1 className="menu-header">PURIFY</h1>
+                <div className="menu-buttons-container">
+                    <Link 
+                        to="/"
+                        className="menu-buttons"
+                    >
+                        HOME
+                    </Link>
+                    <Link 
+                        to="/map"
+                        className="menu-buttons"
+                    >
+                        WATER MAP
+                    </Link>
+                </div>
             </div>
             <p>CA Water Data Challenge</p>
         </div>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../logo.svg';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 // UI
 import Drawer from '@material-ui/core/Drawer';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 
 // COMPONENTS
 import { Menu } from '../components/Menu';
@@ -12,18 +16,44 @@ export const Home = () => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-        <div>
+        <div className="home-page-container">
             <Drawer 
                 anchor='left' 
                 open={menuOpen} 
                 onClose={() => setMenuOpen(false)}
             >
-                <Menu />
+                <div>
+                    <div className="row">
+                        <MenuRoundedIcon
+                            style={{ fontSize: 50 }}
+                            onClick={() => setMenuOpen(false)}
+                        ></MenuRoundedIcon>
+                    </div>
+                    <div className="row">
+                        <img src={logo} className="menu-logo" />
+                        <h1 className="menu-header">PURIFY</h1>
+                        <div className="menu-buttons-container">
+                            <Link to="/" className="menu-buttons">HOME</Link>
+                            <Link to="/map" className="menu-buttons">WATER MAP</Link>
+                        </div>
+                    </div>
+                    <p>CA Water Data Challenge</p>
+                </div>
             </Drawer>
-            <button onClick={() => setMenuOpen(true)}>MENU</button>
-            <h1>HOME</h1>
+            <MenuRoundedIcon 
+                onClick={() => setMenuOpen(true)}
+                style={{ fontSize: 60 }}
+            ></MenuRoundedIcon>
+            <h1 className="home-header-tag">
+                Native American communities in California have a 30% higher risk of poor water quality than that of non-native communities.
+            </h1>
             <iframe id="igraph" scrolling="no" style={{ border:"none" }} seamless="seamless" src="https://plotly.com/~amotiani22/33.embed" height="525" width="100%"></iframe>
-            <Link to="/map">GO TO MAP</Link>
+            <Link to="/map" className="home-button">
+                SEE INTERACTIVE MAP 
+                <NavigateNextIcon
+                    style={{ fontSize: 40 }}
+                />
+            </Link>
         </div>
     )
 }
